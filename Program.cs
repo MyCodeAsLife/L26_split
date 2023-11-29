c13eab144247bd65008f4e0f1f75343110d9ea78
Vladimir <w112vovan@mail.ru>
2023-11-29 19:29:18 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L26_split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "Если бы, да кабы, да во рту росли грибы";
+            string[] stringArray = userInput.Split(' ');
+
+            foreach (string str in stringArray)
+                Console.WriteLine(str);
+        }
+    }
+}
